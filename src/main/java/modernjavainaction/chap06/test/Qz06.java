6460e0f9f4e959096bb37c3a7bc065ddf58cd626
notebook <yeogunna@gmail.com>
2021-08-02 17:15:33 +0900

    Collector groupingBy partitioningBy Custom Collector

--- src/main/java/modernjavainaction/chap06/test/Qz06.java
+++ src/main/java/modernjavainaction/chap06/test/Qz06.java
@@ -1,12 +1,19 @@
 package modernjavainaction.chap06.test;
 
 import modernjavainaction.chap06.Dish;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
 import static modernjavainaction.chap06.Dish.menu;
 import static java.util.stream.Collectors.*;
 
 public class Qz06 {
     public static void main(String[] args) {
-        qz0601CollectorReducing();
+//        qz0601CollectorReducing();
+//        System.out.println(partitionPrimes(20));
+        collectorTest();
     }
 
     public static void qz0601CollectorReducing(){
@@ -19,6 +26,27 @@ public class Qz06 {
 //        3
         shortMenu = menu.stream().collect(reducing("",Dish::getName,(s1,s2)->s1+s2));
         System.out.println(shortMenu);
+
+    }
+
+    public static Map<Boolean, List<Integer>> partitionPrimes(int n){
+        return IntStream.rangeClosed(2,n)
+                .boxed()
+                .collect(partitioningBy(Qz06::isPrime));
+    }
+
+    public static boolean isPrime(int candidate){
+        int candidateRoot = (int) Math.sqrt(candidate);
+        System.out.println("candidate:" + candidate +", candidateRoot :" + candidateRoot);
+        return IntStream.rangeClosed(2,candidateRoot)
+                .noneMatch(i -> candidate%i ==0);
+    }
+
+    public static void collectorTest(){
+        menu.stream()
+                .collect(new ToListCollectorTest<>())
+                .forEach(System.out::println);
+
     }
 
 }
