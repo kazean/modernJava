a9c06f3336592cd87946d05c827c6a00f05dc0d7
notebook <yeogunna@gmail.com>
2021-10-13 19:25:38 +0900

    ch15,16 review

--- src/main/java/modernjavainaction/chap15/test/Pratice.java
+++ src/main/java/modernjavainaction/chap15/test/Pratice.java
@@ -1,29 +1,68 @@
 package modernjavainaction.chap15.test;
 
 import java.util.concurrent.*;
-
-import static modernjavainaction.chap15.Functions.f;
-import static modernjavainaction.chap15.Functions.g;
+import java.util.function.IntConsumer;
 
 public class Pratice {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+//        pracCallback();
+//        pracComepletableFuture();
+        pracComepletableFutrueAdvanced();
+    }
+
+    public static void pracComepletableFutrueAdvanced() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(3);
         int x = 1337;
         CompletableFuture<Integer> a = new CompletableFuture<>();
-        executorService.submit(()-> a.complete(f(x)));
-        int b = g(x);
-//        2674 + 1338 > 4012
-         System.out.println(a.get() + b);
-        executorService.shutdown();
-
-        ExecutorService  executorService1 = Executors.newFixedThreadPool(10);
-        CompletableFuture<Integer> b1 = new CompletableFuture<>();
-        CompletableFuture<Integer> b2 = new CompletableFuture<>();
-        CompletableFuture<Integer> b3 = b1.thenCombine(b2, (y,z)->y+z);
-        executorService1.submit(()-> b1.complete(f(x)));
-        executorService1.submit(()->b2.complete(g(x)));
-        System.out.println("CFCombine result >> " + b3.get());
-        executorService1.shutdown();
-
+        CompletableFuture<Integer> b = new CompletableFuture<>();
+        CompletableFuture<Integer> c = a.thenCombine(b, (y,z)-> y+z);
+        service.submit(()->a.complete(f(x)));
+        service.submit(()->b.complete(g(x)));
+        System.out.println(c.get());
     }
+
+    public static void pracComepletableFuture() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        int x = 1337;
+        CompletableFuture<Integer> a = new CompletableFuture<>();
+        service.submit(()->a.complete(f(x)));
+        System.out.println(a.get()+g(x));
+    }
+
+    public static void pracCallback(){
+        int x = 1337;
+        Result result = new Result();
+
+        callBackF(x, (int y)->{
+            result.left = y;
+            System.out.println(result.left+result.right);
+        });
+
+        callBackG(x, (int y)->{
+            result.right=y;
+            System.out.println(result.left+result.right);
+        });
+    }
+
+    private static class Result{
+        int left;
+        int right;
+    }
+
+    private static int f(int x){
+        return x*2;
+    }
+
+    private static int g(int x){
+        return x+1;
+    }
+
+    private static void callBackF(int x, IntConsumer consumer){
+        consumer.accept(f(x));
+    }
+
+    private static void callBackG(int x, IntConsumer consumer){
+        consumer.accept(g(x));
+    }
+
 }
--- src/main/java/modernjavainaction/chap16/test/Practice.java
+++ src/main/java/modernjavainaction/chap16/test/Practice.java
@@ -1,18 +1,57 @@
 package modernjavainaction.chap16.test;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class Practice {
-    public static void main(String[] args) {
-        CompletableFuture.supplyAsync(()->1);
+    static List<Shop> shopList;
+    static Executor executor;
 
-        Executor executor = Executors.newFixedThreadPool(Math.min(2, 3), new ThreadFactory() {
+    public static void main(String[] args) {
+        /*List<String> item = new ArrayList<>();
+        item.add("iph");
+        item.add("gal");
+
+        item.forEach(s-> {
+            try {
+                System.out.println(s + " : "+getPriceAsync(s).get());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });*/
+
+        shopList = Arrays.asList(new Shop("BestPrice")
+            ,new Shop("LetsSaveBig")
+            ,new Shop("MyFavoriteShop")
+            ,new Shop("BuyItAll")
+                ,new Shop("Temp5")
+                ,new Shop("Temp6")
+                ,new Shop("Temp7")
+                ,new Shop("Temp8")
+                ,new Shop("Temp9"));
+
+//        Stream
+        long start = System.nanoTime();
+        String product = "phone";
+        shopList.stream()
+                .map(s->String.format("%s : %.2f", s.getName(),s.getPrice(product)))
+                .collect(Collectors.toList());
+//                .forEach(s-> System.out.println(s));
+        System.out.println("Duration Stream : " + (System.nanoTime() - start) /1000000+" msec");
+        System.out.println();
+
+//        StreamParallel
+        start = System.nanoTime();
+        shopList.parallelStream()
+                .map(s->String.format("%s : %.2f", s.getName(),s.getPrice(product)))
+                .collect(Collectors.toList());
+//                .forEach(s-> System.out.println(s));
+        System.out.println("Duration Stream Parallel: " + (System.nanoTime() - start) /1000000+" msec");
+        System.out.println();
+
+//        CompletableFuture
+        executor = Executors.newFixedThreadPool(Math.min(shopList.size(), 100), new ThreadFactory() {
             @Override
             public Thread newThread(Runnable r) {
                 Thread t = new Thread(r);
@@ -20,7 +59,58 @@ public class Practice {
                 return t;
             }
         });
+        start = System.nanoTime();
+        List<String> priceList = findPrice(product);
+        System.out.println("Duration : " + (System.nanoTime() - start) /1000000+" msec");
+    }
 
+    public static List<String> findPrice(String product){
+        List<CompletableFuture<String>> priceList = shopList.stream()
+                .map(s-> CompletableFuture.supplyAsync(()->String.format("%s : %.2f", s.getName(), s.getPrice(product)),executor))
+                .collect(Collectors.toList());
+        return priceList.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+    }
 
+    public static CompletableFuture<String> getPriceAsync(String name){
+        return CompletableFuture.supplyAsync(()-> String.format("Price is %.2f",calculate(name)));
+    }
+
+    public static double calculate(String name){
+        try {
+            Thread.sleep(1000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Random random = new Random();
+        return random.nextDouble() * name.charAt(0) + name.charAt(1);
+    }
+}
+
+class Shop{
+    String name;
+
+    public Shop(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice(String product){
+        return calculate(product);
+    }
+
+    public double calculate(String product){
+        try{
+            Thread.sleep(1000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Random random = new Random();
+        return random.nextDouble() * product.charAt(0) + product.charAt(1);
     }
 }
