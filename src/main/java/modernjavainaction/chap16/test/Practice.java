16a47f2110b135c55d38d5a888db60f663b0ccb0
notebook <yeogunna@gmail.com>
2021-10-17 16:09:03 +0900

    ch16 review

--- src/main/java/modernjavainaction/chap16/test/Practice.java
+++ src/main/java/modernjavainaction/chap16/test/Practice.java
@@ -9,6 +9,9 @@ public class Practice {
     static Executor executor;
 
     public static void main(String[] args) {
+        CompletionStage stage;
+        CompletableFuture<String> tmp;
+
         /*List<String> item = new ArrayList<>();
         item.add("iph");
         item.add("gal");
